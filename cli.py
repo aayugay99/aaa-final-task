2c5ba41e1a24fcf209a1386a0618ac24ef4830d6
aayugay99 <sasha.yugay1999@mail.ru>
2022-10-31 16:19:18 +0300

    comments added

--- cli.py
+++ cli.py
@@ -4,58 +4,62 @@ import time
 
 
 class Pizza:
+    """Базовый класс для пиццы, который наследуется видами пиццы."""
     def __init__(self, size: str = 'X'):
         assert size in ['X', 'XL']
-        self.size = size
-        self.max_bake_time = 10 if size == 'XL' else 5
+        self.size = size                                # размер пиццы
+        self.max_bake_time = 10 if size == 'XL' else 5  # максимальное время готовки
 
     @staticmethod
     def dict():
+        """Метод описывает рецепт пиццы. Определяется в наследниках."""
         pass
 
     def __eq__(self, other):
+        """Метод для сравнения пицц. Пиццы сравниваются по размеру и рецепту"""
         if self.dict() == other.dict() and self.size == other.size:
             return True
         return False
 
 
 class Margherita(Pizza):
+    """Класс описывающий Маргариту."""
     @staticmethod
     def dict():
+        """Метод описывает рецепт пиццы."""
         return {'Margherita \U0001F9C0': ['tomato sauce', 'mozzarella', 'tomatoes']}
 
 
 class Pepperoni(Pizza):
+    """Класс описывающий Пепперони. У данной пиццы отличается время готовки."""
     def __init__(self, size: str = 'X'):
         super().__init__(size)
         self.max_bake_time = 6 if size == 'XL' else 3
 
     @staticmethod
     def dict():
+        """Метод описывает рецепт пиццы."""
         return {'Pepperoni \U0001F355': ['tomato sauce', 'mozzarella', 'pepperoni']}
 
 
 class Hawaiian(Pizza):
+    """Класс описывающий Гавайи. У данной пиццы отличается время готовки."""
     def __init__(self, size: str = 'X'):
         super().__init__(size)
         self.max_bake_time = 12 if size == 'XL' else 6
 
     @staticmethod
     def dict():
+        """Метод описывает рецепт пиццы."""
         return {'Hawaiian \U0001F34D': ['tomato sauce', 'mozzarella', 'chicken', 'pineapples']}
 
 
-# def log(func):
-#     def wrapper(x):
-#         start = time.time()
-#         value = func(x)
-#         end = time.time()
-#         print(f'{func.__name__} - {end-start}')
-#         return value
-#     return wrapper
-
-
 def log(template):
+    """
+    Декоратор, выводящий время работы функции.
+    Принимает в качестве аргумента шаблон,
+    в который подставляется время работы декорируемой функции.
+    """
     def my_decorator(func):
         def wrapper(x):
             start = time.time()
@@ -98,7 +102,10 @@ def cli():
 @click.option('--size', default='X')
 @click.argument('pizza', nargs=1)
 def order(pizza: str, delivery: bool, size: str):
-    """Готовит и доставляет пиццу"""
+    """
+    Готовит и доставляет пиццу.
+    При заказе несуществующей пиццы предлагает посмотреть меню.
+    """
 
     if pizza in ['Hawaiian', 'Pepperoni', 'Margherita']:
         if pizza == 'Hawaiian':
@@ -113,7 +120,12 @@ def order(pizza: str, delivery: bool, size: str):
         else:
             pickup(baked_pizza)
     else:
-        print(f'Вы заказали: {pizza}.\nТакой пиццы нет в меню.\nЗапустите команду \'python cli.py menu\' чтоб вывести меню.')
+        print(
+            f'Вы заказали: {pizza}.',
+            'Такой пиццы нет в меню.',
+            'Запустите команду \'python cli.py menu\' чтоб вывести меню.',
+            sep='\n'
+        )
 
 
 @cli.command()
